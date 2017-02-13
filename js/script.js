98aafc3091f83e662e11845c6a858e141d119b2b
m-woo <mark.chi.woo@gmail.com>
2017-02-13 14:58:38 -0800

    added extra chicken buttons appearing

--- js/script.js
+++ js/script.js
@@ -3,15 +3,47 @@
 var score = 0;
 $('#scoreContainer').append('<div id="score">' + score + '</div>');
 
+
+
 $('.button').on('click', function(){
 	console.log(score);
 	score++
 	updateScore();
+	firstCondition();
 });
 
 function updateScore() {
 	$('#score').remove()
 	$('#scoreContainer').append('<div id="score">' + score + '</div>');
-	console.log()
 
+	console.log()
+};
+
+function firstCondition() {
+	if(score==5){
+		console.log('works');
+		$('#one')[0].style.display = 'block'
+		console.log($('#one'))
+	}
+	else if(score==10){
+		$('#two')[0].style.display = 'block'
+	}
+	else if(score==15){
+		$('#three')[0].style.display = 'block'
+	}
+	else if(score==20){
+		$('#four')[0].style.display = 'block'
+	}
+	else if(score==25){
+		$('#five')[0].style.display = 'block'
+	}
+	else if(score==30){
+		$('#six')[0].style.display = 'block'
+	}
+	else if(score==35){
+		$('#seven')[0].style.display = 'block'
+	}
+	else if(score==40){
+		$('#eight')[0].style.display = 'block'
+	}
 }
