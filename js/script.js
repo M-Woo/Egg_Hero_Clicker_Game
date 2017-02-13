f9cf19d56f3537969084616d0b8f0d46eaa8dca8
m-woo <mark.chi.woo@gmail.com>
2017-02-13 13:42:14 -0800

    added basic incriments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+
+
 var score = 0;
+$('#scoreContainer').append('<div id="score">' + score + '</div>');
+
+$('.button').on('click', function(){
+	console.log(score);
+	score++
+	updateScore();
+});
+
+function updateScore() {
+	$('#score').remove()
+	$('#scoreContainer').append('<div id="score">' + score + '</div>');
+	console.log()
+
+}
